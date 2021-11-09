93eab54cabe9a2d8605e0a995929af592a8ef065
LeeTato <leetato25@gmail.com>
2021-11-09 10:56:41 -0600

    feat(client,component)addnavigation

---
+++ auth-basic-main/src/client/src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
+import { Cart } from '../../../../../shared/models/cart.model';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+  cart$ : Observable<Cart | null>
+  constructor(private store: Store<AppState>) {
+    this.cart$ = this.store.select(cartSelector)
+   }
+
+  ngOnInit(): void {
+  }
+
+}
