e7dbe6d79d57602e84b2379a6df95cfff9f29721
LeeTato <leetato25@gmail.com>
2021-11-09 11:25:44 -0600

    refactor(client,app)approutingandcomponent

--- auth-basic-main/src/client/src/app/app-routing.module.ts
+++ auth-basic-main/src/client/src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-// import { SocketioService } from './services/socket.service';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { CreateFoodMenuComponent } from './components/create-food-menu/create-food-menu.component';
+import { FoodMenuListComponent } from './components/food-menu-list/food-menu-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageUsersComponent } from './pages/page-users/page-users.component';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+const routes: Routes = [
+  {path: 'login', component: PageLoginComponent},
+  {path: 'createMenu', component: CreateFoodMenuComponent},
+  {path: 'menuList', component: FoodMenuListComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'users', component: PageUsersComponent, canActivate: [AuthGuard], resolve: []},
+  {path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppComponent {
-
-  title = 'client';
-  constructor() {}
-
-  ngOnInit() {
-    // this.socketService.getMessage().subscribe(message => { console.log(message); });
-  }
-}
+export class AppRoutingModule { }
