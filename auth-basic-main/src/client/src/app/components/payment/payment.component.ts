a0b269e3b7e836ad52d833e6825ebcaddaef7084
LeeTato <leetato25@gmail.com>
2021-11-24 13:54:22 -0600

    feat(client)addpayment component

---
+++ auth-basic-main/src/client/src/app/components/payment/payment.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
+import { usersSelector } from 'src/app/store/selectors/user/user.selectors';
+import { Cart } from '../../../../../shared/models/cart.model';
+import { User } from '../../../../../shared/models/user.model';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss']
+})
+export class PaymentComponent implements OnInit {
+  cart$ : Observable<Cart | null>
+  user$ : Observable<User[]>
+  strikeCheckout:any = null;
+
+  constructor(private store: Store<AppState>) {
+    this.cart$ = this.store.select(cartSelector)
+    this.user$ = this.store.select(usersSelector)
+  }
+  ngOnInit() {
+    this.stripePaymentGateway();
+  }
+
+  checkout(amount:number | undefined) {
+    const strikeCheckout = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51IOFc6Lr3AAb1A8aiceu2oP7PmyDHpKYWQGD9F5N7hMtavMGPdYKPQXjsOvQteGO9aOkDsizpQxOmI6dUuw93Irm00tUkv1EZV',
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken)
+        alert('Stripe token generated!');
+      }
+    });
+
+    strikeCheckout.open({
+      name: 'RemoteStack',
+      description: 'Payment widgets',
+      amount: amount! * 100
+    });
+  }
+
+  stripePaymentGateway() {
+    if(!window.document.getElementById('stripe-script')) {
+      const scr = window.document.createElement("script");
+      scr.id = "stripe-script";
+      scr.type = "text/javascript";
+      scr.src = "https://checkout.stripe.com/checkout.js";
+
+      scr.onload = () => {
+        this.strikeCheckout = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_51IOFc6Lr3AAb1A8aiceu2oP7PmyDHpKYWQGD9F5N7hMtavMGPdYKPQXjsOvQteGO9aOkDsizpQxOmI6dUuw93Irm00tUkv1EZV',
+          locale: 'auto',
+          token: function (token: any) {
+            console.log(token)
+            alert('Payment via stripe successfully!');
+          }
+        });
+      }
+
+      window.document.body.appendChild(scr);
+    }
+  }
+
+}
